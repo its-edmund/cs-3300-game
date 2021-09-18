c2a302b280bf25dfd73f3950ca3ceb7f0cbc75aa
Andrew Roach <aroach34@gatech.edu>
2021-09-17 22:36:04 -0400

    updated setup.py for windows and mac

--- setup.py
+++ setup.py
@@ -5,13 +5,18 @@ from urllib.request import urlopen
 
 if platform.system() == 'Linux':
     url = "https://download2.gluonhq.com/openjfx/17.0.0.1/openjfx-17.0.0.1_linux-x64_bin-sdk.zip"
-    zipresp = urlopen(url)
-    temp_javafx_file = open("tempfile.zip", "wb")
-    temp_javafx_file.write(zipresp.read())
-    temp_javafx_file.close()
+elif platform.system() == 'Windows':
+    url = "https://download2.gluonhq.com/openjfx/17.0.0.1/openjfx-17.0.0.1_windows-x64_bin-sdk.zip"
+elif platform.system() == 'Darwin':
+    url = "https://download2.gluonhq.com/openjfx/17.0.0.1/openjfx-17.0.0.1_osx-x64_bin-sdk.zip"
 
-    javafx_file = ZipFile("tempfile.zip")
-    javafx_file.extractall(path = 'lib/')
-    javafx_file.close()
+zipresp = urlopen(url)
+temp_javafx_file = open("tempfile.zip", "wb")
+temp_javafx_file.write(zipresp.read())
+temp_javafx_file.close()
 
-    os.remove("tempfile.zip")
+javafx_file = ZipFile("tempfile.zip")
+javafx_file.extractall(path = 'lib/')
+javafx_file.close()
+
+os.remove("tempfile.zip")
