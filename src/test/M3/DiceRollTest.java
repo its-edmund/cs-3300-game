b10b4d7968402a3a0013492933abe3a5c5602aec
mxinburritos <mxinburritos@gmail.com>
2021-10-17 21:10:36 -0400

    Add second dice test

--- src/test/M3/DiceRollTest.java
+++ src/test/M3/DiceRollTest.java
@@ -7,11 +7,24 @@ import static org.junit.Assert.assertTrue;
 
 public class DiceRollTest {
     @Test
-    public void testDiceRoll() {
+    public void testDiceRoll10Max() {
         int max = 10;
         Dice dice = new Dice(max);
 
-        dice.rollDice();
-        assertTrue(dice.getValue() <= max && dice.getValue() >= 1);
+        for (int i = 0; i < 100; i++) {
+            dice.rollDice();
+            assertTrue(dice.getValue() <= max && dice.getValue() >= 1);
+        }
+    }
+
+    @Test
+    public void testDiceRoll20Max() {
+        int max = 20;
+        Dice dice = new Dice(max);
+
+        for (int i = 0; i < 1000; i++) {
+            dice.rollDice();
+            assertTrue(dice.getValue() <= max && dice.getValue() >= 1);
+        }
     }
 }
